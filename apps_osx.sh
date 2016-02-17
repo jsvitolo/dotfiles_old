7c0bda3f3737172fc7c84454e10851910ec2781b
Jones Vitolo <jsvitolo@gmail.com>
2016-02-17 16:10:54 -0200

    First commit

---
+++ apps_osx.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Brew Extras
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+brew tap choppsv1/term24
+
+brew install coreutils
