56783591d3484b5aa55786c7743e702793325930
Jones Vitolo <jsvitolo@gmail.com>
2016-02-20 18:37:26 -0200

    Add emacs HEAD

--- apps_osx.sh
+++ apps_osx.sh
@@ -25,11 +25,13 @@ brew tap choppsv1/term24
 
 brew install coreutils
 brew install tree
+brew install gnu-sed --with-default-names
 brew cask install iterm2-beta
 brew install htop-osx
 brew cask install vagrant
 brew cask install vmware-fusion7
 brew install pgcli
+brew install emacs --HEAD
 
 # Docker
 brew install https://raw.githubusercontent.com/Homebrew/homebrew/658b09d6767bd22cb38d06982d6b50b53b9e3f61/Library/Formula/docker.rb
