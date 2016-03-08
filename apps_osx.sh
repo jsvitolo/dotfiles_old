3d7f700107669017a70fb8488ef992e6a9ba8cb3
Jones Vitolo <jsvitolo@gmail.com>
2016-03-08 08:28:51 -0300

    add 'brew install ssh-copy-id'

--- apps_osx.sh
+++ apps_osx.sh
@@ -33,6 +33,9 @@ brew cask install vmware-fusion7
 brew install pgcli
 brew install emacs --HEAD
 brew install watchman
+
+# ssh tools
+brew install ssh-copy-id
 # Docker
 brew install https://raw.githubusercontent.com/Homebrew/homebrew/658b09d6767bd22cb38d06982d6b50b53b9e3f61/Library/Formula/docker.rb
 #brew cask install docker-compose
